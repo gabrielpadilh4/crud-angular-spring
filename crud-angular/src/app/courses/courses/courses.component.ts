4348e5ed0cfa69c00d19341a88c176ba0d7867d3
Gabriel Padilha <gpadilha@addi.com>
2021-10-11 20:04:36 -0300

    feat: Courses service created

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -8,11 +9,11 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [{ _id: '1', name: 'Angular', category: 'front-end' }];
+  courses: Course[] = [];
   displayedColumns: string[] = ['name', 'category'];
 
-  constructor() {
-    // this.courses = [];
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
   }
 
   ngOnInit(): void {}
